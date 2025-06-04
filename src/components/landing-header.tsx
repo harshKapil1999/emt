354c3248b53c240f8bbb738a0b88e4d8d3a3ca37
Harsh Kapil <kapilharsh1999@gmail.com>
2025-06-04 17:50:22 +0530

    -sidebar installation and implementation.

--- src/components/landing-header.tsx
+++ src/components/landing-header.tsx
@@ -4,9 +4,11 @@ import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export function LandingHeadder() {
     const { setTheme } = useTheme()
+    const router = useRouter()
 
     return (
         <header className="w-full flex items-center justify-around p-4 gap-4">
@@ -23,8 +25,12 @@ export function LandingHeadder() {
                 </Link>
                 <div className="hidden md:flex items-center gap-2">
                     <Button variant="blue">Log In</Button>
-                    <Button variant={"outline"}>Sign Up</Button>
-                    
+                    <Button
+                        onClick={() => router.push("/dashboard")}
+                        variant={"outline"}
+                    >
+                        Sign Up
+                    </Button>
                 </div>
                 <Button
                     variant="ghost"
